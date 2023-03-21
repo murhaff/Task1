e800dcd286bb2d43046e0541a1a395e5d2b609c2
ASUS <mourhaf676@gmail.com>
2023-03-21 23:15:23 +0200

    First task

---
+++ src/main/java/org/example/countOccurrences.java
@@ -0,0 +1,13 @@
+package org.example;
+
+class SerialNumberOccurrences {
+    public int countOccurrences(int[] array, int target) {
+        int count = 0;
+        for (int num : array) {
+            if (num == target) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
